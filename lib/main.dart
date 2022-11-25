6fb0ec3e4e64b550e9bd10e8321922b4217009ed
smvillavicencio <smvillavicencio@up.edu.ph>
2022-11-25 14:40:29 +0800

    feat: Connected to the firebase database and auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    MultiProvider(
+      providers: [
+        // ChangeNotifierProvider(create: ((context) => TodoListProvider())),
+        // ChangeNotifierProvider(create: ((context) => AuthProvider())),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
